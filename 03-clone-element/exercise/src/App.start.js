2c211f808517f383393965d484ccf022d55a4fe0
Nike R <nike@apple.com>
2019-02-07 11:24:55 -0800

    Workspace - Day 2 - 1

--- 03-clone-element/exercise/src/App.start.js
+++ 03-clone-element/exercise/src/App.start.js
@@ -5,18 +5,25 @@ import FaForward from "react-icons/lib/fa/forward";
 import FaBackward from "react-icons/lib/fa/backward";
 
 function RadioGroup({ defaultValue, legend, children }) {
+  let [value, setValue] = useState(defaultValue)
+  let clonedElements = React.Children.map(children, child => {
+      return React.cloneElement(child, {
+        _isActive: child.props.value === value,
+        _onSelection: () => setValue(child.props.value)
+      })
+    })
   return (
     <fieldset className="radio-group">
       <legend>{legend}</legend>
-      {children}
+      {clonedElements}
     </fieldset>
   );
 }
 
-function RadioButton({ children }) {
-  const isActive = false; // <-- should come from somewhere
-  const className = "radio-button " + (isActive ? "active" : "");
-  return <button className={className}>{children}</button>;
+function RadioButton({ children, _isActive, _onSelection }) {
+  // const isActive = false; // <-- should come from somewhere
+  const className = "radio-button " + (_isActive ? "active" : "");
+  return <button className={className} onClick={_onSelection}>{children}</button>;
 }
 
 function App() {
